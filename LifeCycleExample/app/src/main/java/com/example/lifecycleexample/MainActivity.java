90a9bc88ef784805ce12a293de51b5b328d025bd
mohan-oss <mohansurendat.mullapudi@gmail.com>
2021-06-12 10:01:26 +0530

    11-06-2021

--- LifeCycleExample/app/src/main/java/com/example/lifecycleexample/MainActivity.java
+++ LifeCycleExample/app/src/main/java/com/example/lifecycleexample/MainActivity.java
@@ -2,50 +2,59 @@ package com.example.lifecycleexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        Log.i("APSSDC","SecondActivityCreated");
+        setContentView(R.layout.activity_main);
+        Log.i("APSSDC","ActivityCreated");
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("APSSDC","SecondActivityCreated");
+        Log.i("APSSDC","ActivityCreated");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("APSSDC","SecondActivityResumed");
+        Log.i("APSSDC","ActivityResumed");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i("APSSDC","SecondActivitypaused");
+        Log.i("APSSDC","Activitypaused");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("APSSDC","SecondActivitystopped");
+        Log.i("APSSDC","Activitystopped");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i("APSSDC","SecondActivityrestarted");
+        Log.i("APSSDC","Activityrestarted");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i("APSSDC","SecondActivityDestroyed");
+        Log.i("APSSDC","ActivityDestroyed");
+    }
+
+    public void next (View view) {
+
+        startActivity (new Intent(  MainActivity . this, SecondActivity . class) );
+
     }
 }
